d953b23f7e561a4b987dd8d997ec6f8a653a32e4
Rodrigo Cericatto <graffiti75@gmail.com>
2025-08-15 21:03:45 -0300

    Added HeaderCart and { user ? () : null } checks on Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import HeaderCart from "./HeaderCart";
+import { useNavigate } from "react-router-dom";
 import { DarkModeContext } from "../context/DarkModeContext";
 import { AuthContext } from "../auth/AuthContext";
 import { CartContext } from "../cart/CartContext";
@@ -33,7 +34,7 @@ const Header: React.FC = () => {
 				</div>
 
 				<div className="flex-1 max-w-2xl">
-					<SearchInput />
+					{user ? <SearchInput /> : null}
 				</div>
 
 				<div className="flex items-center space-x-4">
@@ -50,30 +51,9 @@ const Header: React.FC = () => {
 							</button>
 						</>
 					) : null}
-					<Link
-						to="/cart"
-						className="relative text-gray-600 dark:text-gray-300"
-					>
-						<svg
-							className="h-6 w-6"
-							fill="none"
-							stroke="currentColor"
-							viewBox="0 0 24 24"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth="2"
-								d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-							></path>
-						</svg>
-						{cartItems.length > 0 && (
-							<span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
-								{cartItems.length}
-							</span>
-						)}
-					</Link>
+					{user ? (
+						<HeaderCart cartItemsCount={cartItems.length} />
+					) : null}
 					<button
 						className="bg-gray-200 dark:bg-gray-700 text-black dark:text-white px-3 py-1 rounded"
 						onClick={toggleDarkMode}
---
+++ src/components/HeaderCart.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+interface HeaderCartProps {
+	cartItemsCount: number;
+}
+
+const HeaderCart: React.FC<HeaderCartProps> = ({ cartItemsCount }) => (
+	<Link to="/cart" className="relative text-gray-600 dark:text-gray-300">
+		<svg
+			className="h-6 w-6"
+			fill="none"
+			stroke="currentColor"
+			viewBox="0 0 24 24"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				strokeWidth="2"
+				d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+			></path>
+		</svg>
+		{cartItemsCount > 0 && (
+			<span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
+				{cartItemsCount}
+			</span>
+		)}
+	</Link>
+);
+
+export default HeaderCart;
